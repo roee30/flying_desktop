1730998fd42f332d84b59cc27f9c82a8059ba84b
Roee Nizan <roeen30@gmail.com>
2020-01-01 00:15:30 +0200

    Added: --clean confirmation

--- compile.py
+++ compile.py
@@ -8,9 +8,9 @@ from pathlib import Path
 
 def main():
     os.chdir(Path(__file__).parent)
-    subprocess.run(
-        ["pyinstaller", "--onefile", "--clean", "pyinstaller.spec",]
-    )
+    subprocess.Popen(
+        ["pyinstaller", "--onefile", "--clean", "pyinstaller.spec"]
+    ).communicate("y\n".encode())
 
 
 if __name__ == "__main__":
